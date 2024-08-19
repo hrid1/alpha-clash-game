01014e7bec0db6cd1abc475b9c74feecf9bd7316
hrid1 <hridoycse1@gmail.com>
2024-08-19 11:58:58 +0600

    hide and show element

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Hi play now");
+
+function playGame() {
+  hideElement("home");
+  showElement("play-ground");
+}
---
+++ utilitis.js
@@ -0,0 +1,9 @@
+function hideElement(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.add("hidden");
+}
+
+function showElement(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.remove("hidden");
+}
